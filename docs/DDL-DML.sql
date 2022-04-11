75b80c89da631db2ef0b3019e129ca8e6da9c59e
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-04-11 07:56:42 -0300

    refactor: matching js naming DDL-DML

--- docs/DDL-DML.sql
+++ docs/DDL-DML.sql
@@ -1,28 +1,20 @@
-create database to_do_to_learn;
+create database toDoToLearn;
 
-use to_do_to_learn;
+use toDoToLearn;
 
-create table if not exists usuarios (
-	usuario_id int not null auto_increment,
-    nome varchar(255) not null,
-    email varchar(255) not null,
-    primary key(usuario_id)
+create table if not exists person (
+	id int not null auto_increment,
+    personName varchar(255) not null,
+    email varchar(255) not null unique,
+    primary key(id)
 );
 
-create table if not exists tasks (
-	task_id int not null auto_increment,
-	usuario_id int not null,
-    titulo varchar(255) not null,
-    descricao text,
-    status enum("to-do", "doing", "done") not null,
-    primary key(task_id),
-    foreign key (usuario_id) references usuarios(usuario_id)
+create table if not exists task (
+	id int not null auto_increment,
+	personId int not null,
+    title varchar(255) not null,
+    description text,
+    taskStatus enum("to-do", "doing", "done") not null,
+    primary key(id),
+    foreign key (personId) references person(id)
 );
-
-insert into usuarios (nome, email)
-values (
-    "João",
-    "email_do_joão@gmail.com"
-);
-
-select * from usuarios;
