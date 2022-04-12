e4434fb474c526019df97c8cf33b337bf27a779e
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-04-12 19:09:34 -0300

    fix: imported cors and routes

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
-
+const corsOptions = {
+    origin: "http://localhost:3001",
+};
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.text());
-
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
-
-app.listen(port, () => console.log(`Server is runing on port ${port}...`));
+require("./src/routes/person.routes")(app);
+require("./src/routes/task.routes")(app);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is runing on PORT ${PORT}...`));
