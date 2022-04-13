e06a3a86ef261ef0bcf78bcf41d3b739ca563acb
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-04-13 13:33:59 -0300

    fix(model): created a updateById function for Task

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,6 +1,6 @@
-const Model = require("./model");
-
-class Task extends Model {
+const task = require("./model");
+const sql = require("./db");
+class Task extends task {
     constructor(task) {
         super();
         this.personId = task.personId;
@@ -9,5 +9,25 @@ class Task extends Model {
         this.taskStatus = task.taskStatus;
     }
 }
+
+Task.updateById = function (id, task, result) {
+    sql.query(
+        "UPDATE task SET title = ?, description = ?, taskStatus = ? WHERE id = ?",
+        [task.title, task.description, task.taskStatus, id],
+        (err, res) => {
+            if (err) {
+                console.log(err);
+                result(null, err);
+                return;
+            }
+            if (res.affectedRows == 0) {
+                result({ kind: "not_found" }, null);
+                return;
+            }
+            console.log(`updated task: `, { id: id, ...task });
+            result(null, { id: id, ...task });
+        }
+    );
+};
 
 module.exports = Task;
